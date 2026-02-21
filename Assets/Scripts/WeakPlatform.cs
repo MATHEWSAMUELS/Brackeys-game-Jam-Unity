4a1376ca683c6a70c853518f7e396f5e60e80994
MATHEWSAMUELS <132983700+MATHEWSAMUELS@users.noreply.github.com>
2026-02-21 15:43:29 +0530

    PlayStart

--- Assets/Scripts/WeakPlatform.cs
+++ Assets/Scripts/WeakPlatform.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Tilemaps; 
 
 public class WeakPlatform : MonoBehaviour
 {
     public float collapseTime = 1.5f;
 
     private bool isCollapsing = false;
-    private BoxCollider2D col;
-    private SpriteRenderer sr;
+    private CompositeCollider2D compositeCol; 
+    private TilemapCollider2D tilemapCol; // Added to catch the raw collider
+    private TilemapRenderer tmr;    
 
     void Start()
     {
-        col = GetComponent<BoxCollider2D>();
-        sr = GetComponent<SpriteRenderer>();
+        // Get ALL the components involved in the platform
+        compositeCol = GetComponent<CompositeCollider2D>();
+        tilemapCol = GetComponent<TilemapCollider2D>();
+        tmr = GetComponent<TilemapRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,19 +31,22 @@ public class WeakPlatform : MonoBehaviour
     {
         isCollapsing = true;
 
-        // Optional: visual warning
-        if (sr != null)
-            sr.color = Color.red;
+        // 1. Visual Warning (Red)
+        if (tmr != null)
+        {
+            tmr.material.color = Color.red;
+        }
 
         yield return new WaitForSeconds(collapseTime);
 
-        // Disable platform permanently
-        col.enabled = false;
+        // 2. Disable Visuals
+        if (tmr != null)
+        {
+            tmr.enabled = false;
+        }
 
-        if (sr != null)
-            sr.enabled = false;
-
-        // If you want it to fully disappear:
-        // gameObject.SetActive(false);
+        // 3. Disable BOTH Colliders to ensure nothing is blocking the player
+        if (compositeCol != null) compositeCol.enabled = false;
+        if (tilemapCol != null) tilemapCol.enabled = false;
     }
 }
